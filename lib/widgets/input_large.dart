267097b06c4d7b0f1be63bd316fa59f34cb21dd8
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2023-11-01 00:16:39 -0300

    oxi nao commitou kkk

--- lib/widgets/input_large.dart
+++ lib/widgets/input_large.dart
@@ -1,34 +1,66 @@
 import 'package:flutter/material.dart';
 
-class InputLarge extends StatelessWidget {
+class InputLarge extends StatefulWidget {
   final String labelText;
   final TextEditingController? controller;
   final Function(String)? onChanged;
   final TextInputType? keyboardType;
-  final bool password;
+  final bool obscureText;
 
   InputLarge({
     required this.labelText,
     this.controller,
     this.onChanged,
     this.keyboardType,
-    this.password = false,
+    this.obscureText = false,
   });
+
+  @override
+  _InputLargeState createState() => _InputLargeState();
+}
+
+class _InputLargeState extends State<InputLarge> {
+  bool _obscureText = true; // Variável de estado para controlar a visualização
+  FocusNode _focusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode.addListener(_handleFocusChange);
+  }
+
+  void _handleFocusChange() {
+    setState(() {}); // Reconstruir o widget quando o foco mudar
+  }
+
+  void _togglePasswordView() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+
+  @override
+  void dispose() {
+    _focusNode.removeListener(_handleFocusChange);
+    _focusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return TextField(
-      obscureText: password,
-      controller: controller,
-      onChanged: onChanged,
-      keyboardType: keyboardType,
+      focusNode: _focusNode,
+      controller: widget.controller,
+      onChanged: widget.onChanged,
+      keyboardType: widget.keyboardType,
       cursorColor: Colors.yellow,
+      obscureText: widget.obscureText ? _obscureText : false,
       style: TextStyle(color: Colors.white),
       decoration: InputDecoration(
         contentPadding: EdgeInsets.symmetric(vertical: 25.0, horizontal: 10.0),
         filled: true,
         fillColor: Color.fromARGB(255, 58, 58, 58),
-        labelText: labelText,
+        labelText: widget.labelText,
         labelStyle: TextStyle(color: Colors.white),
         enabledBorder: OutlineInputBorder(
           borderSide: BorderSide.none,
@@ -38,6 +70,15 @@ class InputLarge extends StatelessWidget {
           borderSide: BorderSide(color: Colors.yellow, width: 2.0),
           borderRadius: BorderRadius.circular(12.0),
         ),
+        suffixIcon: widget.obscureText && _focusNode.hasFocus
+            ? IconButton(
+                icon: Icon(
+                  _obscureText ? Icons.visibility_off : Icons.visibility,
+                  color: Colors.white,
+                ),
+                onPressed: _togglePasswordView,
+              )
+            : null,
       ),
     );
   }
